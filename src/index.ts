f98d93a1e4cde11c84aba227c6e0f50543bbf383
Damian <przpiw@gmail.com>
2021-09-15 18:08:38 +0930

    implemented generic collections

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
-import { Collection } from "./models/Collection";
-import { User, UserProps } from "./models/User";
+import { User } from "./models/User";
 
 const collection = User.buildUserCollection();
-
 
 collection.on('change', () => {
     console.log(collection)
--- src/models/Attributes.ts
+++ src/models/Attributes.ts
@@ -1,7 +1,5 @@
-import { UserProps } from "./User";
 export class Attributes<T> {
   constructor(private data: T) { }
-
 
   get = <K extends keyof T>(key: K): T[K] => {
     return this.data[key]
